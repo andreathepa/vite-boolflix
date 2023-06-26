094bc2def4f93bda1ed302f52e318d50e908741d
andreathepa <andreadepa32@gmail.com>
2023-06-26 16:59:41 +0200

    boilerplate

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,32 @@
 <script>
-export default {
+import { store } from './store.js';
+import axios from 'axios';
 
+import HeaderApp from './HeaderApp.vue';
+
+export default {
+  components:{
+      HeaderApp,
+    },
+    data(){
+        return{
+          store,
+        }
+    
+    },
+    mounted(){
+        axios.get(store.apiUrl).then((response) => {
+           store.movies = response.data.results;
+           
+        })
+    }
 }
 </script>
 <template lang="">
   <div>
-    
+    <HeaderApp />
   </div>
 </template>
-<style lang="">
-  
+<style lang="scss">
+@use './styles/generals.scss' as *;
 </style>
---
+++ src/HeaderApp.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+    
+}
+</script>
+<template lang="">
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col">
+                <input type="text" placeholder="Cerca qui il tuo film">
+                <button class="btn btn-primary p-2 mx-2">Search</button>
+
+            </div>
+
+        </div>
+    </div>
+</template>
+<style lang="scss" scoped>
+    
+</style>
